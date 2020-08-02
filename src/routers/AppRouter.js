617c2f84ee3ac88cb0009e74b9d7678b0610d738
juancho108 <jjf108@gmail.com>
2020-08-01 21:37:43 -0300

    subiendo rutas completas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,24 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { AuthRouter } from "./AuthRouter";
+import { JournalScreen } from "../components/journal/JournalScreen";
 
 export const AppRouter = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/auth" component={AuthRouter} />
+          <Route exact path="/" component={JournalScreen} />
+
+          <Redirect to="/auth/login" />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
